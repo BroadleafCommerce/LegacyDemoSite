f23dd8c3a1789805959d5320e97c3983681ac82a
Andre Azzolini <apazzolini@gmail.com>
2012-05-24 16:35:59 -0500

    Move Thymeleaf dependencies to Broadleaf, play around with custom dialects

--- site-war/src/main/webapp/index.jsp
+++
@@ -1,3 +0,0 @@
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-Welcome to a Broadleaf Application!
-<%-- Redirected because we can't set the welcome page to a virtual URL. --%>
---
+++ site/src/main/java/com/mycompany/controller/DefaultController.java
@@ -0,0 +1,37 @@
+package com.mycompany.controller;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.broadleafcommerce.core.catalog.domain.Category;
+import org.broadleafcommerce.core.catalog.service.CatalogService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DefaultController {
+	
+	@Resource(name = "blCatalogService")
+	protected CatalogService catalogService;
+
+	@ModelAttribute("secret")
+	public Integer theSecret() {
+		return 12344;
+	}
+	
+	@ModelAttribute("allCategories")
+	public List<Category> allCategories() {
+		return catalogService.findAllCategories();
+	}
+
+	@RequestMapping("/")
+	public String home(HttpServletRequest request, HttpServletResponse response) {
+		System.out.println(catalogService);
+		return "index";
+	}
+
+}
