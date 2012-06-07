c5b7a2303fa0555b73b277fbcd3c7d8a04898740
Andre Azzolini <apazzolini@gmail.com>
2012-06-07 18:02:09 -0500

    Initial category page view

--- site/src/main/java/com/mycompany/controller/DefaultController.java
+++ site/src/main/java/com/mycompany/controller/DefaultController.java
@@ -7,9 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.broadleafcommerce.core.catalog.domain.Category;
+import org.broadleafcommerce.core.catalog.domain.Product;
 import org.broadleafcommerce.core.catalog.service.CatalogService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,19 +19,19 @@ public class DefaultController {
 	@Resource(name = "blCatalogService")
 	protected CatalogService catalogService;
 
-	@ModelAttribute("secret")
-	public Integer theSecret() {
-		return 12344;
-	}
-	
-	@ModelAttribute("allCategories")
-	public List<Category> allCategories() {
-		return catalogService.findAllCategories();
-	}
-
 	@RequestMapping("/")
 	public String home(HttpServletRequest request, HttpServletResponse response) {
 		return "index";
+	}
+	
+	@RequestMapping("/hot-sauces")
+	public String hotSauces(HttpServletRequest request, HttpServletResponse response, Model model) {
+		Category category = catalogService.findCategoriesByName("Hot Sauces").get(0);
+		List<Product> products = catalogService.findProductsForCategory(category);
+		
+		model.addAttribute("products", products);
+		
+		return "category";
 	}
 
 }
