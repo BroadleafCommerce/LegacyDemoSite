d4351449ef7111bfabb91b4aea2ba028dcf75054
Andre Azzolini <apazzolini@gmail.com>
2012-06-12 11:51:48 -0500

    Improved UX for add to cart

--- site/src/main/webapp/js/script.js
+++ site/src/main/webapp/js/script.js
@@ -1,13 +1,14 @@
 $(function(){
 	$('.add_to_cart a').click(function(){
+		var link = this;
 		$.ajax(this.href, {
 			statusCode: {
 				200: function(data) {
-					$('#notification_bar').empty().append("The item has been added to the cart!").slideToggle('slow').delay('3500').slideToggle('slow');
-					$('#cartItemsCount').html(data);
+					$(link).html('In Cart').attr('href', $('#cartLink').attr('href')).parent().removeClass('add_to_cart').addClass('in_cart');
+					$('#notification_bar').html("The item has been added to the cart!").slideToggle('slow').delay('3500').slideToggle('slow');
 			    }, 
 			    500: function() {
-					$('#notification_bar').empty().append("ERROR ADDING").slideToggle('slow').delay('3500').slideToggle('slow');
+					$('#notification_bar').html("Error adding :( Please try again").slideToggle('slow').delay('3500').slideToggle('slow');
 			    }
 			  }
 		});
@@ -15,7 +16,7 @@ $(function(){
 	});
 });
 
-$(".various").fancybox({
+$(".fancycart").fancybox({
 	maxWidth	: 940,
 	maxHeight	: 570,
 	fitToView	: false,
