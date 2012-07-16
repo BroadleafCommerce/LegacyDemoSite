397274d84743a8afbef21d93ce004bbfb66217f9
Elbert Bautista <ebautista@broadleafcommerce.org>
2012-07-15 23:25:16 -0500

    updating checkout flow to break out billing and shipping into separate partials. incorporated the shipping validation and updated thyme leaf templates accordingly.

--- site/src/main/java/com/mycompany/controller/checkout/CheckoutController.java
+++ site/src/main/java/com/mycompany/controller/checkout/CheckoutController.java
@@ -1,10 +1,19 @@
 package com.mycompany.controller.checkout;
 
+import org.apache.commons.validator.GenericValidator;
+import org.broadleafcommerce.core.order.domain.Order;
+import org.broadleafcommerce.core.payment.domain.PaymentInfo;
+import org.broadleafcommerce.core.payment.service.type.PaymentInfoType;
+import org.broadleafcommerce.core.web.checkout.model.BillingInfoForm;
 import org.broadleafcommerce.core.web.checkout.model.OrderMultishipOptionForm;
-import org.broadleafcommerce.core.web.checkout.model.ShippingAddressForm;
+import org.broadleafcommerce.core.pricing.service.exception.PricingException;
+import org.broadleafcommerce.core.web.checkout.model.ShippingInfoForm;
 import org.broadleafcommerce.core.web.controller.checkout.BroadleafCheckoutController;
+import org.broadleafcommerce.core.web.order.CartState;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -18,11 +27,62 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping("/checkout")
 public class CheckoutController extends BroadleafCheckoutController {
 
+    /*
+    * The Checkout page for Heat Clinic will have the shipping information pre-populated with an address if the fulfillment group has an address and fulfillment option associated with it.
+    * It also assumes that there is only one payment info of type credit card on the order. If so, then the billing address will be pre-populated.
+    */
     @RequestMapping("")
-	public String checkout(HttpServletRequest request, HttpServletResponse response, Model model) {
-		return super.checkout(request, response, model);
+	public String checkout(@ModelAttribute("shippingInfoForm") ShippingInfoForm shippingForm,
+                           @ModelAttribute("billingInfoForm") BillingInfoForm billingForm,
+            HttpServletRequest request, HttpServletResponse response, Model model) {
+        Order cart = CartState.getCart();
+        if (cart.getFulfillmentGroups().get(0).getAddress() != null) {
+            shippingForm.setAddress(cart.getFulfillmentGroups().get(0).getAddress());
+            shippingForm.setFulfillmentOptionId(cart.getFulfillmentGroups().get(0).getFulfillmentOption().getId());
+        }
+
+        for (PaymentInfo paymentInfo : cart.getPaymentInfos()) {
+            if (PaymentInfoType.CREDIT_CARD == paymentInfo) {
+                billingForm.setAddress(paymentInfo.getAddress());
+            }
+        }
+
+        return super.checkout(request, response, model);
 	}
-    
+
+    /*
+      * The Heat Clinic requires that Shipping Address Email be filled out and that a State be selected.
+      * Note: In BroadleafCommerce, State is optional to handle international addresses.
+      * We will add any custom validations after calling super.saveSingleShippingAddress()
+      *
+      * Notice that we also include billingInfoForm in the request in case of any validation errors since
+      * this is a single page checkout.
+      * Spring EL will know how to bind the forms on the page accordingly.
+       *
+      */
+    @RequestMapping(value="/singleship", method = RequestMethod.POST)
+    public String saveSingleShip(HttpServletRequest request, HttpServletResponse response, Model model,
+            @ModelAttribute("billingInfoForm") BillingInfoForm billingForm,
+            @ModelAttribute("shippingInfoForm") ShippingInfoForm shippingForm, BindingResult result) throws PricingException {
+        String path = super.saveSingleShip(request, response, model, shippingForm, result);
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(result, "address.emailAddress", "emailAddress.required");
+        if (!GenericValidator.isEmail(shippingForm.getAddress().getEmailAddress())) {
+            result.rejectValue("address.emailAddress", "emailAddress.invalid", null, null);
+        }
+
+        if (shippingForm.getAddress().getState() == null) {
+            result.rejectValue("address.state", "state.required", null, null);
+        }
+
+        if (result.hasErrors()) {
+            checkout(request, response, model);
+            return isAjaxRequest(request) ? "ajax/checkout" : "/checkout";
+        }
+
+        return  path;
+    }
+
     @RequestMapping(value = "/multiship", method = RequestMethod.GET)
     public String showMultiship(HttpServletRequest request, HttpServletResponse response, Model model) {
     	return super.showMultiship(request, response, model);
@@ -41,7 +101,7 @@ public class CheckoutController extends BroadleafCheckoutController {
     
     @RequestMapping(value = "/add-address", method = RequestMethod.POST)
     public String saveMultishipAddAddress(HttpServletRequest request, HttpServletResponse response, Model model,
-    		@ModelAttribute("addressForm") ShippingAddressForm addressForm) {
+    		@ModelAttribute("addressForm") ShippingInfoForm addressForm) {
     	return super.saveMultishipAddAddress(request, response, model, addressForm);
     }
     
