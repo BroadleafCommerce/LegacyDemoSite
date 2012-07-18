37c1ac4a14987e6e1aa2618c72b6a5f186737ef3
Joseph Fridye <jfridye@gmail.com>
2012-07-18 17:10:15 -0500

    Order History updates.

---
+++ site/src/main/webapp/js/manageAccountOperations.js
@@ -0,0 +1,22 @@
+/* Operations that deal with the customer's account */
+$(function(){
+
+    // Bind all links with class .view-order-details to toggle the order details
+    $('body').on('click', 'a.view-order-details', function() {
+        var url = $(this).attr('href');
+        var $orderDetails = $(this).parents('.order').find('.order-details');
+        
+        // if this is the currently opened order details, collapse it
+        if ($orderDetails.is(':visible')) {
+        	$orderDetails.slideToggle();
+        } else {
+        	$orderDetails.load(url, function() {
+        		$visibleSections = $('.order-details:visible');
+        		$orderDetails.slideToggle();
+        		$visibleSections.slideToggle();
+        	});
+        }	
+        return false;
+    });
+
+});
