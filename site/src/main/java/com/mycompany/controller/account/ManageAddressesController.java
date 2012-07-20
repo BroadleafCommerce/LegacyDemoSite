3d86e3c1d9a4aa57e940506ec782f3dd02e68ec2
Andre Azzolini <apazzolini@gmail.com>
2012-07-20 08:21:12 -0500

    site was failing to startup

--- site/src/main/java/com/mycompany/controller/account/ManageAddressesController.java
+++ site/src/main/java/com/mycompany/controller/account/ManageAddressesController.java
@@ -1,18 +1,16 @@
 package com.mycompany.controller.account;
 
 import org.broadleafcommerce.core.web.controller.account.BroadleafManageAddressesController;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-@Controller
-@RequestMapping("/account/addresses")
+//@Controller
+//@RequestMapping("/account/addresses")
 public class ManageAddressesController extends BroadleafManageAddressesController {
 
+	/*
+	 * 
+	 *  there is already managecustomeraddresscontroller. these two had conflicting bean defintions
+	 * 
+	 * 
     @RequestMapping(method = RequestMethod.GET)
     public String viewManageAddresses(HttpServletRequest request, HttpServletResponse response, Model model) {
         return super.viewManageAddresses(request, response, model);
@@ -22,5 +20,6 @@ public class ManageAddressesController extends BroadleafManageAddressesControlle
     public String processManageAddresses(HttpServletRequest request, HttpServletResponse response, Model model) {
         return super.processManageAddresses(request, response, model);
     }
+    */
 
 }
