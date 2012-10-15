59540bd6d477a179beca73aa4baabb442f6f7f08
Jerry Ocanas <jerry77oz@gmail.com>
2012-10-15 10:30:29 -0500

    Fixed issue with switching locales while on the search results page.

--- site/src/main/webapp/js/BLC.js
+++ site/src/main/webapp/js/BLC.js
@@ -50,11 +50,41 @@ var BLC = (function($) {
 	    });
 	    return o;
 	}
+
+    /**
+     * Add a URL parameter (or changing it if it already exists)
+     * @param {search} string  this is typically document.location.search
+     * @param {key}    string  the key to set
+     * @param {val}    string  value
+     */
+    function addUrlParam(search, key, val){
+        var newParam = key + '=' + val,
+            params = '?' + newParam;
+
+        // If the "search" string exists, then build params from it
+        if (search) {
+            // Try to replace an existing instance
+            params = search.replace(new RegExp('[\?]' + key + '[^&]*'), newParam);
+
+            // If nothing was replaced, then check if it exists as a trailing param
+            if (params === search) {
+                params = search.replace(new RegExp('[\&]' + key + '[^&]*'), '&' + newParam);
+
+                // If nothing was replaced and the key is not already present, then add the new param to the end
+                if ((params === search) && (search.indexOf(val) == -1) ) {
+                    params += '&' + newParam;
+                }
+            }
+        }
+
+        return document.location.search = params;
+    };
 	
 	return {
 		redirectIfNecessary : redirectIfNecessary,
 		getExtraData : getExtraData,
 		ajax : ajax,
-		serializeObject : serializeObject
-	}
+		serializeObject : serializeObject,
+        addUrlParam : addUrlParam
+    }
 })($);
