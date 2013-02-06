8f6b309275e5a745e37955c1538e364a8789dbb7
Andre Azzolini <apazzolini@gmail.com>
2013-02-06 15:41:19 -0600

    Add initial support for showing modals for adorned target lists

--- admin/src/main/webapp/js/admin/components/listGrid.js
+++ admin/src/main/webapp/js/admin/components/listGrid.js
@@ -1,73 +1,107 @@
 $(document).ready(function() {
 	
-	// Allow clicking on the entire table row to navigate to the entity record
-	$('.listGridContainer tbody').on('click', 'tr', function() {
-		window.location = $(this).data('link');
+	/**
+	 * Bind a handler to trigger anytime a table row is clicked on any list grid. 
+	 * 
+	 * After assembling information, this will delegate to the specialized rowSelected
+	 * handler for this particular kind of list grid.
+	 */
+	$('body').on('click', '.list-grid-table tbody tr', function() {
+		var $tr = $(this);
+		var $table = $tr.closest('table');
+		var link = $tr.data('link');
+		var listGridType = $table.data('listgridtype');
+		var fields = {};
+		
+		$tr.find('td').each(function() {
+			var fieldName = $(this).data('fieldname');
+			var value = $(this).text();
+			fields[fieldName] = value;
+		});
+		
+		$('body').trigger('listGrid-' + listGridType + '-rowSelected', [link, fields]);
 	});
 	
-    $('body').on('click', 'a.subCollectionListGridAdd', function(event) {
-		$.get($(this).attr('href'), function(data) {
-			$modal = $(data);
-			
-			$modal.attr('id', 'andre');
-			$('body').append($modal);
-			$modal.modal();
-			
-			$modal.on('hidden', function() {
-				$modal.remove();
-			});
-		});
-		return false;
-    });
+	/**
+	 * The rowSelected handler for the main list grid simply needs to navigate to the given URL.
+	 */
+	$('body').on('listGrid-main-rowSelected', function(event, link, fields) {
+		window.location = link;
+	});
 	
+	/**
+	 * The rowSelected handler for a toOne list grid needs to trigger the specific valueSelected handler 
+	 * for the field that we are performing the to-one lookup on.
+	 */
+	$('body').on('listGrid-toOne-rowSelected', function(event, link, fields) {
+		$('div.additional-foreign-key-container').trigger('valueSelected', fields);
+	});
 	
+	/**
+	 * The rowSelected handler for a simpleCollection list grid ...
+	 */
+	$('body').on('listGrid-basicCollection-rowSelected', function(event, link, fields) {
+		alert('handling row selected for a collection add');
+	});
 	
+	/**
+	 * The rowSelected handler for an adornedTarget list grid ...
+	 */
+	$('body').on('listGrid-adornedTarget-rowSelected', function(event, link, fields) {
+		alert('handling row selected for an adornedTarget add');
+	});
 	
-	
-	
-	
-	
-	var $modal;
-	var $textInput;
-	var $hiddenInput;
-	var displayValProp;
-	
-    $('body').on('click', 'a.showModalListGrid', function(event) {
-    	event.preventDefault();
+	/**
+	 * This handler will fire for additional foreign key fields when the find button is clicked.
+	 * 
+	 * It is responsible for binding a valueSelected handler for this field as well as launching
+	 * a list grid modal that will be used to select the to-one entity.
+	 * 
+	 * Note that we MUST unbind this handler when the modal is hidden as there could be many different
+	 * to-one fields on an entity form.
+	 */
+    $('body').on('click', 'a.show-to-one-list-grid', function(event) {
+    	var $container = $(this).closest('div.additional-foreign-key-container');
     	
-    	$textInput = $(this).prev();
-    	$hiddenInput = $textInput.prev();
-    	displayValProp = $hiddenInput.prev().val();
-		
-		$.get($(this).attr('href'), function(data) {
-			$modal = $(data);
-			
-			$modal.attr('id', 'andre');
-			$('body').append($modal);
-			$modal.modal();
-			
-			$modal.on('hidden', function() {
-				$modal.remove();
-			});
-		});
-		
+    	// This handler will
+    	$container.on('valueSelected', function(event, fields) {
+    		debugger;
+    		var $this = $(this);
+    		var displayValueProp = $this.find('input.display-value-property').val();
+    		
+    		$this.find('input.value').val(fields['id']);
+    		$this.find('input.display-value').val(fields[displayValueProp]);
+    		
+    		$('#modal').modal('hide');
+    	});
+    	
+    	showLinkAsModal($(this).attr('href'), function() {
+			$('div.additional-foreign-key-container').unbind('valueSelected');
+    	});
+    	
 		return false;
     });
-    
-    $('body').on('click', 'div.modal tbody tr', function(event) {
-		var entityId = $(this).find('td').filter(function() { 
-			return $(this).data('fieldname') == 'id'; 
-		}).text();
-		
-		var displayVal = $(this).find('td').filter(function() { 
-			return $(this).data('fieldname') == displayValProp; 
-		}).text();
-		
-		$modal.modal('hide');
-		
-		$textInput.val(displayVal);
-		$hiddenInput.val(entityId);
-    });
-
+	
+	$('body').on('click', 'a.sub-list-grid-add', function() {
+    	showLinkAsModal($(this).attr('href'));
+		return false;
+	});
+	
+	var showLinkAsModal = function(link, callback) {
+		$.get(link, function(data) {
+			var $data = $(data);
+			
+			$('body').append($data);
+			$data.modal();
+			
+			$data.on('hidden', function() {
+				if (callback != null) {
+					callback();
+				}
+				$(this).remove();
+			});
+		});
+	};
+	
 });
 
