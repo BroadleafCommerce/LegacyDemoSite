179ef3f4768649e6b9557845e3c92c864ba0145c
Joshua Skorton <joshua.skorton@gmail.com>
2014-05-11 17:57:23 -0500

    #105.  Update cart removal and update quantity for sku browsing.

--- site/src/main/webapp/js/cartOperations.js
+++ site/src/main/webapp/js/cartOperations.js
@@ -162,7 +162,11 @@ $(function(){
                 if (extraData) {
                     updateHeaderCartItemsCount(extraData.cartItemCount);
                     if ($form.children('input.quantityInput').val() == 0) {
-                        showAddToCartButton(extraData.productId, 'cart');
+                    	if(extraData.skuId != null) {
+                        	showAddToCartButton(extraData.skuId, 'cart');
+                    	} else {
+                    		showAddToCartButton(extraData.productId, 'cart');
+                    	}
                     }
                 }
 
@@ -181,7 +185,12 @@ $(function(){
                 type: "GET"
             }, function(data, extraData) {
                 updateHeaderCartItemsCount(extraData.cartItemCount);
-                showAddToCartButton(extraData.productId, 'cart');
+                showAddToCartButton(extraData.productId, 'cart');               
+                if(extraData.skuId != null) {
+                	showAddToCartButton(extraData.skuId, 'cart');
+            	} else {
+            		showAddToCartButton(extraData.productId, 'cart');
+            	}
                 
                 $('.simplemodal-wrap').html(data);
             }
@@ -190,7 +199,7 @@ $(function(){
     });
     
     // Intercept remove from cart operations and perform them via AJAX instead
-    // This will trigger on any link with class "remove_from_cart"
+    // This will trigger on any link with class "remove_promo"
     $('body').on('click', 'a.remove_promo', function() {
         var link = this;
         
