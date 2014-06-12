cccec9beef4051d0179f52afc4a9e1023a01ff5a
Brian Polster <bpolster@broadleafcommerce.org>
2014-06-12 14:09:04 -0500

    #34 Updated JavaScript and Product detail page.

--- site/src/main/webapp/js/cartOperations.js
+++ site/src/main/webapp/js/cartOperations.js
@@ -98,6 +98,12 @@ $(function(){
             	
             	if ("TEXT" == optionType) {
             		value = $(element).next().find('input').val();
+            	} else if ("TEXTAREA" == optionType) {
+            		value = $(element).next().find('textarea').val();
+            	} else if ("DECIMAL" == optionType) {
+            		value = $(element).next().find('input').val();
+            	} else if ("SELECT" == optionType) {
+            		value = $(element).next().find('select option:checked').val();
             	} else {
             		value = $(element).text();
             	}//need to add other types(date,long, etc) as needed
