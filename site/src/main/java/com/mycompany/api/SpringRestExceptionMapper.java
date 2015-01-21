3707bb85a1ce4a3263959aac5252b96ac9219b06
Chris Kittrell <ckittrell@broadleafcommerce.com>
2015-01-21 10:03:44 -0600

    broadleafcommerce/broadleafcommerce#1221 - Extension of BroadleafSpringRestExceptionMapper so that the bean is defined in the api's servlet rather than the root application context

---
+++ site/src/main/java/com/mycompany/api/SpringRestExceptionMapper.java
@@ -0,0 +1,9 @@
+package com.mycompany.api;
+
+import org.broadleafcommerce.core.web.api.BroadleafSpringRestExceptionMapper;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class SpringRestExceptionMapper extends BroadleafSpringRestExceptionMapper {
+    
+}
