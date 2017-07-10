5f395352d780b6045d89c6c9198da1d3f99aea4e
Phillip Verheyden <pverheyden@gmail.com>
2017-07-10 08:09:13 -0500

    Remove unnecessary beans and annotations for 5.2

--- site/src/main/java/com/mycompany/api/RestApiMvcConfiguration.java
+++ site/src/main/java/com/mycompany/api/RestApiMvcConfiguration.java
@@ -2,13 +2,11 @@ package com.mycompany.api;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.broadleafcommerce.rest.api.BroadleafRestApiMvcConfiguration;
 
 
 @Configuration
-@EnableWebMvc
 @ComponentScan("com.mycompany.api")
 public class RestApiMvcConfiguration extends BroadleafRestApiMvcConfiguration {
     
